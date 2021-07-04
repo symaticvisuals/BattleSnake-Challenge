9c2e217fe5402ebb731095d4193cf273a8810c80
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-07-05 03:42:10 +0530

    make it better

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -102,7 +102,7 @@ public class Snake {
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
             response.put("author", "");           // TODO: Your Battlesnake Username
-            response.put("color", "#888888");     // TODO: Personalize
+            response.put("color", "#000");     // TODO: Personalize
             response.put("head", "default");  // TODO: Personalize
             response.put("tail", "default");  // TODO: Personalize
             return response;
